7ccbbb0e8813f7c045164c9be2de4227249440bb
Cole Morgan <colemmorgann@gmail.com>
2025-01-23 03:46:42 -0500

    Events updates

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,9 +7,9 @@ export default function Events() {
     <>
       <section className="relative min-h-screen bg-[#080d14]">
         <div className="noise-bg"/>
-        <div className="mx-auto max-w-7xl px-4 md:px-10 pt-24 pb-12 text-white">
-          <MonthlyEvents />
-          <Calendar />
+        <div className="mx-auto max-w-7xl px-4 md:px-10 pt-20 sm:pt-32 pb-12 text-white">
+        <Calendar />
+          <MonthlyEvents /> 
         </div>
       </section>
     </>
