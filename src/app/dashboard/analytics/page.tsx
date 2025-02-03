0e5871ab1d18b17f89ea45a5332cee4e1b880dcb
Claudio Sciotto <claudiogsdb@gmail.com>
2025-02-03 10:52:20 -0500

    Added analytics

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import ProtectedRoute from "@/components/admin/auth/ProtectedRoute";
-import AnalyticsDashboard from "@/components/admin/analytics/AnalyticsDashboard";
+import { Analytics } from "@vercel/analytics/react";
 
-export default function Analytics() {
+export default function AnalyticsDashboard() {
   return (
     <ProtectedRoute>
-      <div className="mx-auto max-w-7xl px-4 pt-8 sm:px-6 sm:pt-16 lg:px-10">
-        <h1 className="mb-8 text-2xl sm:text-3xl">Analytics</h1>
-        <AnalyticsDashboard />
-      </div>
+      <Analytics />
     </ProtectedRoute>
   );
 }
