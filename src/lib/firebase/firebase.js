ea112af81a855ad684f226404349bcdd1627f567
Claudio Sciotto <claudiogsdb@gmail.com>
2025-02-03 14:00:04 -0500

    fixed **

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,6 +24,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 
-export { db, auth, app, storage, analytics };
+export { db, auth, app, storage };
