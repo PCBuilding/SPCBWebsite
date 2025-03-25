e563f29f55eff2e7018816122bba623a51a9a3d8
Claudio Sciotto <fisizion@gmail.com>
2025-03-25 16:18:07 -0400

    update about section in landing page
    moved the tube.png img higher for consitency

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -36,12 +36,11 @@ const About: React.FC = () => {
   const isInView = useInView(h3Ref, { once: true });
 
   return (
-    <div className="pb-20 pt-16 sm:pt-32 max-w-7xl mx-auto px-10 overflow-hidden relative">
-      <div className="about-fade absolute left-0 right-0 top-0 h-10 z-20" />
+    <div className="relative mx-auto max-w-7xl overflow-hidden px-10 pt-16 sm:pt-32">
       <motion.img
         src="/landing/tube.png"
         alt=""
-        className={`hidden sm:block absolute -top-48 z-0 left-1/2 -translate-x-1/2 overflow-hidden transition-all ${
+        className={`absolute -top-64 left-1/2 z-0 hidden -translate-x-1/2 overflow-hidden transition-all sm:block ${
           isInView ? "scale-x-100" : "scale-x-[0.25]"
         } duration-1000`}
         initial={{ opacity: 0 }}
@@ -50,7 +49,10 @@ const About: React.FC = () => {
       />
 
       <div className="relative z-10">
-        <h3 ref={h3Ref} className="text-[40px] font-medium pb-6 sm:pb-10 text-center">
+        <h3
+          ref={h3Ref}
+          className="pb-6 text-center text-[40px] font-medium sm:pb-10"
+        >
           Why SPCB?
         </h3>
         <ul>
@@ -69,18 +71,18 @@ interface AboutItemProps {
 
 const AboutItem: React.FC<AboutItemProps> = ({ info }) => {
   return (
-    <li className="py-12 sm:py-16 border-b border-[#1a2a3d] gap-8 grid md:grid-cols-2">
-      <div className="flex flex-col items-center text-center md:text-left md:items-start justify-between gap-3 sm:gap-4">
-        <p className="text-3xl sm:text-4xl lg:text-4xl leading-[1.2] xl:leading-[1.2] balance max-w-[320px] sm:max-w-[410px]">
+    <li className="grid gap-8 border-b border-[#1a2a3d] py-12 sm:py-16 md:grid-cols-2">
+      <div className="flex flex-col items-center justify-between gap-3 text-center sm:gap-4 md:items-start md:text-left">
+        <p className="balance max-w-[320px] text-3xl leading-[1.2] sm:max-w-[410px] sm:text-4xl lg:text-4xl xl:leading-[1.2]">
           {info.title}
         </p>
-        <p className="text-lg lg:text-xl text-dull max-w-[445px] text-balance">
+        <p className="max-w-[445px] text-balance text-lg text-dull lg:text-xl">
           {info.desc}
         </p>
       </div>
 
       <div className="flex justify-center md:justify-end">
-        <div className="bg-[#090f1a] border border-[#1a2a3d] rounded-xl w-full max-w-96 aspect-square overflow-hidden">
+        <div className="aspect-square w-full max-w-96 overflow-hidden rounded-xl border border-[#1a2a3d] bg-[#090f1a]">
           {info.children}
         </div>
       </div>
